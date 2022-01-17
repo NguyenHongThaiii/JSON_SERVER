5916812610887e6490b2605eba01e2e1d0246292
Nguyen Thai <tranvotam123@gmail.com>
2022-01-17 23:40:44 +0700

    Update imageUrl for categories

--- data/category-data.js
+++ data/category-data.js
@@ -1,10 +1,34 @@
 const categoryListTemp = [
-  { name: "Pants" },
-  { name: "Varsity" },
-  { name: "Tee" },
-  { name: "Hoodie" },
-  { name: "Shirt" },
-  { name: "Accessory" },
+  {
+    name: "Pants",
+    imageUrl:
+      "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-1.png",
+  },
+  {
+    name: "Varsity",
+    imageUrl:
+      "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-2.png",
+  },
+  {
+    name: "Tee",
+    imageUrl:
+      "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-3.png",
+  },
+  {
+    name: "Hoodie",
+    imageUrl:
+      "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-4.png",
+  },
+  {
+    name: "Shirt",
+    imageUrl:
+      "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-5.png",
+  },
+  {
+    name: "Accessory",
+    imageUrl:
+      "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-6.webp",
+  },
 ];
 
 module.exports = categoryListTemp;
