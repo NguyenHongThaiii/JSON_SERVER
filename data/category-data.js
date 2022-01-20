9adb2ad955d5f7965b71c26f43b8d17164efaeef
Nguyen Thai <tranvotam123@gmail.com>
2022-01-20 17:24:27 +0700

    Add subtitle for category list

--- data/category-data.js
+++ data/category-data.js
@@ -3,31 +3,37 @@ const categoryListTemp = [
     name: "Pants",
     imageUrl:
       "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-1.png",
+    subTitle: "Style yourself with these unique pants",
   },
   {
     name: "Varsity",
     imageUrl:
       "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-2.png",
+    subTitle: "Shine in your style",
   },
   {
     name: "Tee",
     imageUrl:
       "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-3.png",
+    subTitle: "Simple but not ordinary",
   },
   {
     name: "Hoodie",
     imageUrl:
       "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-4.jpg",
+    subTitle: "Create your own winter style",
   },
   {
     name: "Shirt",
     imageUrl:
       "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-5.png",
+    subTitle: "Go sporty this summer with this vintage navy",
   },
   {
     name: "Accessory",
     imageUrl:
       "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-6.webp",
+    subTitle: "Small things sometimes help us a lot",
   },
 ];
 
