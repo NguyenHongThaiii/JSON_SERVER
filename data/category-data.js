fcbbd91314d46b2099d1c910df048c1236cca984
Nguyen Thai <tranvotam123@gmail.com>
2022-01-20 18:36:11 +0700

    update slide for category list

--- data/category-data.js
+++ data/category-data.js
@@ -4,36 +4,48 @@ const categoryListTemp = [
     imageUrl:
       "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-1.png",
     subTitle: "Style yourself with these unique pants",
+    slide:
+      "https://js-ecommerce-api.herokuapp.com/asset/img/category_slide_1.jpg",
   },
   {
     name: "Varsity",
     imageUrl:
       "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-2.png",
     subTitle: "Shine in your style",
+    slide:
+      "https://js-ecommerce-api.herokuapp.com/asset/img/category_slide_1.jpg",
   },
   {
     name: "Tee",
     imageUrl:
       "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-3.png",
     subTitle: "Simple but not ordinary",
+    slide:
+      "https://js-ecommerce-api.herokuapp.com/asset/img/category_slide_1.jpg",
   },
   {
     name: "Hoodie",
     imageUrl:
       "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-4.jpg",
     subTitle: "Create your own winter style",
+    slide:
+      "https://js-ecommerce-api.herokuapp.com/asset/img/category_slide_1.jpg",
   },
   {
     name: "Shirt",
     imageUrl:
       "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-5.png",
     subTitle: "Go sporty this summer with this vintage navy",
+    slide:
+      "https://js-ecommerce-api.herokuapp.com/asset/img/category_slide_1.jpg",
   },
   {
     name: "Accessory",
     imageUrl:
       "https://js-ecommerce-api.herokuapp.com/asset/img/categories_image-6.webp",
     subTitle: "Small things sometimes help us a lot",
+    slide:
+      "https://js-ecommerce-api.herokuapp.com/asset/img/category_slide_1.jpg",
   },
 ];
 
